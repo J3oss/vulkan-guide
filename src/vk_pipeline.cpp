b3f561dd5b94253ab55a7cdd8a099316cb2d3d19
youssefassem <youssef.assem.mohamed@gmail.com>
2021-02-08 18:22:47 +0200

    pipeline builder

---
+++ src/vk_pipeline.cpp
@@ -0,0 +1,46 @@
+#include "vk_pipeline.h"
+#include "iostream"
+
+VkPipeline PipelineBuilder::build_pipeline(VkDevice device, VkRenderPass pass)
+{
+	VkPipelineViewportStateCreateInfo viewportState = {};
+	viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	viewportState.pNext = nullptr;
+
+	viewportState.viewportCount = 1;
+	viewportState.pViewports = &viewport;
+	viewportState.scissorCount = 1;
+	viewportState.pScissors = &scissor;
+
+	VkPipelineColorBlendStateCreateInfo colorBlendState = {};
+	colorBlendState.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+	colorBlendState.pNext = nullptr;
+
+	colorBlendState.logicOpEnable = VK_FALSE;
+	colorBlendState.logicOp = VK_LOGIC_OP_COPY;
+	colorBlendState.attachmentCount = 1;
+	colorBlendState.pAttachments = &colorBlendAttachmentState;
+
+	VkGraphicsPipelineCreateInfo pipelineInfo = {};
+	pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+	pipelineInfo.pNext = nullptr;
+
+	pipelineInfo.stageCount = shaderStages.size();
+	pipelineInfo.pStages = shaderStages.data();
+	pipelineInfo.pVertexInputState = &vertexInputState;
+	pipelineInfo.pInputAssemblyState = &inputAssemblyState;
+	pipelineInfo.pViewportState = &viewportState;
+	pipelineInfo.pRasterizationState = &rasterizerState;
+	pipelineInfo.pMultisampleState = &multisampleState;
+	pipelineInfo.pColorBlendState = &colorBlendState;
+
+	pipelineInfo.layout = pipelineLayout;
+	pipelineInfo.renderPass = pass;
+	pipelineInfo.subpass = 0;
+	pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+	VkPipeline newPipeline;
+	VK_CHECK(vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &newPipeline));
+
+	return newPipeline;
+}
---
+++ src/vk_pipeline.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vk_types.h>
+#include "vk_engine.h"
+#include <vector>
+
+class PipelineBuilder
+{
+public:
+
+  std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+  VkPipelineVertexInputStateCreateInfo vertexInputState;
+  VkPipelineInputAssemblyStateCreateInfo inputAssemblyState;
+  VkViewport viewport;
+  VkRect2D scissor;
+  VkPipelineRasterizationStateCreateInfo rasterizerState;
+  VkPipelineColorBlendAttachmentState colorBlendAttachmentState;
+  VkPipelineMultisampleStateCreateInfo multisampleState;
+  
+  VkPipelineLayout pipelineLayout;
+
+  VkPipeline build_pipeline(VkDevice device, VkRenderPass renderPass);
+
+private:
+
+};
