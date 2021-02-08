04a6877cc0c3f297ab392d70f4381f2e048cbae2
youssefassem <youssef.assem.mohamed@gmail.com>
2021-02-08 18:29:19 +0200

    pipeline itializers

--- src/vk_types.h
+++ src/vk_types.h
@@ -1,8 +1,10 @@
-﻿// vulkan_guide.h : Include file for standard system include files,
-// or project specific include files.
-
 #pragma once
 
 #include <vulkan/vulkan.h>
 
-//we will add our main reusable types here
+#define VK_CHECK(x)\
+do { \
+		VkResult err = x; \
+		if(err) \
+		{ std::cout <<"Detected Vulkan error: " << err << std::endl; abort(); }\
+} while(0);
